ce46f720b86ee10057493c81b683f93a36ed094f
alvaro7913 <alvarocesped@gmail.com>
2022-04-12 18:58:00 -0400

    first commit

---
+++ peliculas.sql
@@ -0,0 +1,23 @@
+\c alvaro
+DROP DATABASE peliculas;
+CREATE DATABASE peliculas;
+\c peliculas
+
+-- Cargar ambos archivos a su tabla correspondiente. (1 Punto)
+CREATE TABLE peliculas(
+    id SERIAL,  -- este es el campo princial de la tabla 
+    Pelicula VARCHAR(70), -- es un campo de varchar  
+    agnoEstreno, 
+    Director
+)
+
+
+-- 3. Obtener el ID de la película “Titanic”. (1 Punto)
+-- 4. Listar a todos los actores que aparecen en la película "Titanic". (1 Puntos)
+-- 5. Consultar en cuántas películas del top 100 participa Harrison Ford. (2 Puntos)
+-- 6. Indicar las películas estrenadas entre los años 1990 y 1999 ordenadas por título de
+-- manera ascendente. (1 punto)
+-- 7. Hacer una consulta SQL que muestre los títulos con su longitud, la longitud debe ser
+-- nombrado para la consulta como “longitud_titulo”. (1 punto)
+-- 8. Consultar cual es la longitud más grande entre todos los títulos de las películas.
+-- (2 punto)
